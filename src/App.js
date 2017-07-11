fc7df10ad19b872652b711c049f4b89dd99ee90e
Iury Krieger <iurykrieger96@gmail.com>
2017-07-11 15:37:48 -0300

    Raw layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div id="root">
+      <div className="main">
+        <Header/>
+        <Timeline/>
       </div>
+    </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/reset.css';
+import './css/timeline.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
